755838689e8c447c4a6aec813cd2716b23f1795b
Kate Ray <email@kateray.net>
2013-02-23 20:43:05 -0500

    god this makes me crazy

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,19 +22,18 @@ class User < ActiveRecord::Base
   end
 
   def get_overlaps(x)
+    checkins = self.checkins
+    friends = self.friends
     colors = ['#f62323', '#f6b423', '#57d11b', '#06754a', '#3fc8e4', '#3f7ae4', '#603fe4', '#a73fe4', '#e43fa2', '#8e1e00', '#8e5b00', '#048e00', '#00768e', '#00278e', '#47008e', '#8e006e', '#ff7c7c', '#b0fbb4', '#b0fbf8', '#b0c4fb', '#ecb0fb', '#fbb0b6', '#490c03', '#493c03', '#264903', '#03493a', '#031f49', '#230349', '#490345', '#49031e']
     
-    puts '*'*80
-    puts x
     x.to_i.times do |num|
-      puts 'umm'
       offset = num*100
       #TODO - Pretty up url
       options = {:query => {:v => '20130214', :sort => 'newestfirst', :oauth_token => self.atoken, :offset => offset.to_s}}
       response = HTTParty.get("https://api.foursquare.com/v2/users/#{foursquare_id.to_s}/historysearch", options)
       response.parsed_response['response']['checkins']['items'].each do |i|
         if i['overlaps']
-          unless self.checkins.find{|c| c.foursquare_id == i['id']}
+          unless checkins.find{|c| c.foursquare_id == i['id']}
             checkin = Checkin.create! do |c|
               c.user_id = self.id
               c.foursquare_id = i['id']
@@ -43,17 +42,19 @@ class User < ActiveRecord::Base
               c.venue_id = i['venue']['id']
               c.venue_name = i['venue']['name']
             end
+            checkins << checkin
             i['overlaps']['items'].each do |item|
-              unless friend = self.friends.find{|f| f.foursquare_id == item['user']['id']}
+              unless friends.find{|f| f.foursquare_id == item['user']['id'].to_s}
                 friend = Friend.create! do |f|
                   color = colors[0]
                   f.color = color
                   colors = colors.drop(1)
                   colors << color
                   f.user_id = self.id
-                  f.foursquare_id = item['user']['id']
+                  f.foursquare_id = item['user']['id'].to_s
                   f.name = item['user']['firstName'] + ' ' + (item['user']['lastName'] || '')
                 end
+                friends << friend
               end
               overlap = Overlap.create! do |o|
                 o.friend_id = friend.id
